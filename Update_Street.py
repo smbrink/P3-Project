9676e878598f09b5dc4994a795d1f91adffc6c56
smbrink <smbrink@swbell.net>
2017-03-29 17:14:27 -0500

    Delete Update_Street.py

--- Update_Street.py
+++
@@ -1,5 +0,0 @@
-def update_name(name, mapping):
-    for search_error in mapping:
-        if search_error in name:
-            name = re.sub(r'\b' + search_error + r'\b\.?', mapping[search_error], name)
-    return name
